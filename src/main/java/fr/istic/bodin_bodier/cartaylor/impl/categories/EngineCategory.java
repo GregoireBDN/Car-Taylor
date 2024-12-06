f83281a783fb3255903aa3580350524a93e2ee71
Léo Bernard-Bodier <leobrnbdr@dn032148.etu.univ-rennes1.fr>
2024-12-06 17:25:39 +0100

    Ajouts propriétés exterior & engine

--- src/main/java/fr/istic/bodin_bodier/cartaylor/impl/categories/EngineCategory.java
+++ src/main/java/fr/istic/bodin_bodier/cartaylor/impl/categories/EngineCategory.java
@@ -3,11 +3,29 @@ package fr.istic.bodin_bodier.cartaylor.impl.categories;
 import fr.istic.bodin_bodier.cartaylor.impl.AbstractCategory;
 
 public class EngineCategory extends AbstractCategory {
-  public EngineCategory() {
-    super("Engine");
+
+  public enum FuelType {
+    Gasoline,
+    Diesel,
+    Hybrid
   }
 
   // Propriétés spécifiques aux moteurs
   private int power; // en kW
-  private String fuelType; // "Gasoline", "Diesel", "Hybrid"
+  private FuelType fuelType; // "Gasoline", "Diesel", "Hybrid"
+
+  public EngineCategory() {
+    super("Engine");
+    this.power = 80;
+    this.fuelType = FuelType.Diesel;
+  }
+
+
+  public FuelType getFuelType() {
+    return fuelType;
+  }
+
+  public int getPower() {
+    return power;
+  }
 }
--- src/main/java/fr/istic/bodin_bodier/cartaylor/impl/categories/ExteriorCategory.java
+++ src/main/java/fr/istic/bodin_bodier/cartaylor/impl/categories/ExteriorCategory.java
@@ -5,9 +5,23 @@ import fr.istic.bodin_bodier.cartaylor.impl.AbstractCategory;
 public class ExteriorCategory extends AbstractCategory {
   public ExteriorCategory() {
     super("Exterior");
+    this.color = Color.BLACK;
+    this.paintType = "Classic";
   }
 
+   
+  public enum Color {
+    RED,
+    BLUE,
+    GREEN,
+    BLACK,
+    WHITE,
+    GREY
+  }
+
+  private Color color;
   // Propriétés spécifiques à l'extérieur
   private String paintType; // "Classic", "Metallic", "Sport"
-  private String color;
+ 
+  
 }
